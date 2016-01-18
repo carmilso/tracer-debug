ce06f634286f5ebc926692c51ff41fa5f3ce8ee0
Carlos <carmilso@inf.upv.es>
2016-01-18 17:22:11 +0100

    bug fixed

--- tracer-debug.js
+++ tracer-debug.js
@@ -7,51 +7,54 @@ function TLogger(options) {
 
   this.log = function() {
     if (isDebug) {
-      var line = new Error().stack.split("\n")[2].match(/(\(.*)\)/i)[0];
+      var line = new Error().stack.match(/(at .*)/i)[0];
       var args = Array.prototype.slice.call(arguments);
-      tracer.log("%j in %s", args, line);
+      tracer.log(line);
     }
   };
 
   this.trace = function() {
     if (isDebug) {
-      var line = new Error().stack.split("\n")[2].match(/(\(.*)\)/i)[0];
+      var line = new Error().stack.match(/(at .*)/i)[0];
       var args = Array.prototype.slice.call(arguments);
-      tracer.trace("%j in %s", args, line);
+      tracer.log(line);
     }
   };
 
   this.debug = function() {
     if (isDebug) {
-      var line = new Error().stack.split("\n")[2].match(/(\(.*)\)/i)[0];
+      var line = new Error().stack.match(/(at .*)/i)[0];
       var args = Array.prototype.slice.call(arguments);
-      tracer.debug("%j in %s", args, line);
+      tracer.log(line);;
     }
   };
 
   this.info = function() {
     if (isDebug) {
-      var line = new Error().stack.split("\n")[2].match(/(\(.*)\)/i)[0];
+      var line = new Error().stack.match(/(at .*)/i)[0];
       var args = Array.prototype.slice.call(arguments);
-      tracer.info("%j in %s", args, line);
+      tracer.log(line);
     }
   };
 
   this.warn = function() {
     if (isDebug) {
-      var line = new Error().stack.split("\n")[2].match(/(\(.*)\)/i)[0];
+      var line = new Error().stack.match(/(at .*)/i)[0];
       var args = Array.prototype.slice.call(arguments);
-      tracer.warn("%j in %s", args, line);
+      tracer.log(line);
     }
   };
 
   this.error = function() {
     if (isDebug) {
-      var line = new Error().stack.split("\n")[2].match(/(\(.*)\)/i)[0];
+      var line = new Error().stack.match(/(at .*)/i)[0];
       var args = Array.prototype.slice.call(arguments);
-      tracer.error("%j in %s", args, line);
+      tracer.log(line);
     }
   };
 };
 
+/**
+* @version 1.0.5
+*/
 module.exports = TLogger;
