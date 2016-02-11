74ddaab6279385e5c235f87e6553476a3d8454f2
Luis A. Leiva <lleiva@sciling.com>
2016-02-11 14:26:07 +0100

    Updated examples

--- prove.js
+++ prove.js
@@ -2,34 +2,32 @@
 
 // This is a simple usage example.
 
-var TLogger;
+var TracerDebug;
 try {
   // NPM installs: use the node_modules file.
-  TLogger = require('tracer-debug');
+  TracerDebug = require('tracer-debug');
 } catch(err) {
   // Local test: use the local file.
-  TLogger = require('./tracer-debug');
+  TracerDebug = require('./tracer-debug');
 }
 
-var options = {
-  format      : "{{timestamp}} <{{title}}> {{message}}",
-  dateformat  : "HH:MM:ss.L",
-  stackTrace  : true
-}
+// Just customize a couple of options.
+var logger = new TracerDebug({
+  format: "<{{title}}> {{message}}",
+  stackTrace: true
+});
 
-var tlogger = new TLogger(options);
-
-tlogger.log(42);
+logger.log(42);
 
 // Notice the stack trace.
 (function test() {
-  tlogger.trace('An object:', { foo: 1, bar: { a: 2, b: 3 } });
+  logger.trace('An object:', { foo: 1, bar: { a: 2, b: 3 } });
 })();
 
-tlogger.debug(true, false);
+logger.debug(true, false);
 
-tlogger.info(1/0, 0/0);
+logger.info(1/0, 0/0);
 
-tlogger.warn(null, undefined);
+logger.warn(null, undefined);
 
-tlogger.error("foo", typeof "bar");
+logger.error("foo", typeof "bar");
