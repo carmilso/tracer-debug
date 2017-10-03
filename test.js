1e894429bd4be98c4926b4849a1a627f4efc0932
Luis Leiva <lleiva@sciling.com>
2017-10-03 12:11:42 +0200

    Updated test

--- test.js
+++ test.js
@@ -3,15 +3,7 @@
 // This is a simple usage example.
 console.log("** BEGIN TEST:", process.argv.join(' '), "ENV:", process.env.NODE_ENV, "\n");
 
-
-var TracerDebug;
-try {
-  // NPM installs: use the node_modules file.
-  TracerDebug = require('tracer-debug');
-} catch(err) {
-  // Local test: use the local file.
-  TracerDebug = require('./index');
-}
+var TracerDebug = require('./index');
 
 // Just customize a couple of options.
 var logger = new TracerDebug({
@@ -19,10 +11,16 @@ var logger = new TracerDebug({
   format: "logger/{{title}}: {{message}}",
   // Display last 2 lines from stack.
   stackTrace: 2,
+  // Use a global instance to reuse them later.
   singleton: true
 });
 
 logger.log(42);
+
+// In singleton mode, the output is the same as in the previous `logger` instance.
+// If we don't use `singleton: true` a brand new instance is created.
+var logger2 = new TracerDebug({ singleton: true });
+logger2.log(42);
 
 // Notice the stack trace.
 (function test() {
@@ -30,6 +28,7 @@ logger.log(42);
 })();
 
 // The following examples are just to show the colored output.
+// It also showcases how native types are displayed.
 logger.debug(true, false);
 logger.info(1/0, 0/0);
 logger.warn(null, undefined);
@@ -39,9 +38,8 @@ logger.error(new Error("An error has been thrown."));
 logger.transport.setLevel('warn');
 logger.log('This message should not be shown!');
 logger.warn('hello %s! %j', 'world', { foo: 1 });
+// Notice that in singleton mode any modification is populated to other instances.
+logger2.log('This message should not be shown!');
 
-var logger2 = new TracerDebug({ singleton: true });
-// Notice that the output is the same as in the previous `logger` instance.
-logger2.log(42);
 
 console.log("\n** END OF TEST");
